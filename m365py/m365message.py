ddfb6404edb4e804b7ab94de2c1c6c771e8c6c4f
AntonHakansson <anton.hakansson98@gmail.com>
2019-07-03 12:28:51 +0200

    Seperated into package

---
+++ m365py/m365message.py
@@ -0,0 +1,242 @@
+from enum import Enum
+import struct
+
+def phex(s):
+    return ''.join('/x{:02x}'.format(x) for x in s)
+
+HEADER = 0xAA55
+
+class Direction(Enum):
+    MASTER_TO_MOTOR      = 0x20
+    MASTER_TO_BATTERY    = 0x22
+    MOTOR_TO_MASTER      = 0x23
+    BATTERY_TO_MASTER    = 0x25
+
+class ReadWrite(Enum):
+    READ  = 0x01
+    WRITE = 0x03
+
+class Attribute(Enum):
+    GENERAL_INFO           = 0x10 # serial, pin, fwversion
+    # GENERAL_INFO_EXTENDED  = 0x10 # TODO: serial, fwversion, totalcapacity, cycles, chargingtimes, productiondate
+    DISTANCE_LEFT          = 0x25
+    BATTERY_INFO           = 0x31 # remaining cap, percent, current, voltage, temperature
+    BATTERY_PERCENT        = 0x32
+    BATTERY_CURRENT        = 0x33
+    BATTERY_VOLTAGE        = 0x34
+    BATTERY_CELL_VOLTAGES  = 0x40 # cell voltages (Cells 1-10)
+    TRIP_INFO              = 0x3A # ontime, trip_distance_m?(seems to increment if throttle is applied), frametemp1
+    SPEED                  = 0xB5
+    TRIP_DISTANCE          = 0xB9
+    CRUISE                 = 0x7C
+    TAIL_LIGHT             = 0x7D
+    MOTOR_INFO             = 0xB0 # error warning flags workmode battery_percent speed_kmh speed_average_kmh odometer_km trip_distance_m uptime_s frame_temperature
+    SUPPLEMENTARY          = 0x7B # kers, cruisemode, taillight
+
+class ParseStatus:
+    OK               = 'ok'
+    DISJOINTED       = 'disjointed'
+    INVALID_HEADER   = 'invalid_header'
+    INVALID_CHECKSUM = 'invalid_checksum'
+
+class Message:
+
+    def __init__(self):
+        self._direction  = None
+        self._read_write = None
+        self._attribute  = None
+        self._payload    = None
+
+        self._checksum  = None
+        self._raw_bytes  = None
+
+    def as_dict(self): return self.__dict__
+
+    def direction(self, direction):
+        self._direction = direction
+        return self
+
+    def read_write(self, read_write):
+        self._read_write = read_write
+        return self
+
+    def attribute(self, attribute) :
+        self._attribute = attribute
+        return self
+
+    # For now payload is stored in little endian format
+    def payload(self, payload):
+        self._payload = payload
+        return self
+
+    def _calc_checksum(self):
+        checksum = 0
+        checksum += self._direction.value
+        checksum += self._read_write.value
+        checksum += self._attribute.value
+
+        for byte in self._payload:
+            checksum += byte
+        checksum += len(self._payload) + 2
+        checksum ^= 0xffff
+        checksum &= 0xffff
+
+        self._checksum = checksum
+
+
+    def build(self):
+        self._calc_checksum()
+
+        result = bytearray()
+        result.extend(struct.pack('<H', HEADER))
+        # >>>> these are single byte so we don't have to worry about byte order
+        result.append(len(self._payload) + 2)
+        result.append(self._direction.value)
+        result.append(self._read_write.value)
+        result.append(self._attribute.value)
+        # <<<<
+        result.extend(self._payload) # TODO: store payload as big endian in class
+        result.extend(struct.pack('<H', self._checksum))
+
+        self._raw_bytes = bytes(result)
+        return self
+
+    @staticmethod
+    def parse_from_bytes(message):
+        message_length = len(message)
+        payload_start = 6
+        header, length, direction, read_write, attribute = struct.unpack('<HBBBB', message[:payload_start])
+        payload_end = payload_start + length - 2
+
+        if header != HEADER:              return ParseStatus.INVALID_HEADER , None
+        if payload_end > message_length:  return ParseStatus.DISJOINTED     , None
+
+        direction  = Direction(direction)
+        read_write = ReadWrite(read_write)
+        attribute  = Attribute(attribute)
+        payload    = message[payload_start:payload_end]
+
+        result = Message()          \
+            .direction(direction)   \
+            .read_write(read_write) \
+            .attribute(attribute)   \
+            .payload(payload)       \
+            .build()
+
+        if message[:-2] !=  result._raw_bytes[:-2]:
+            return ParseStatus.INVALID_CHECKSUM, None
+
+        return ParseStatus.OK, result
+
+battery_voltage = Message()                       \
+    .direction(Direction.MASTER_TO_BATTERY)       \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.BATTERY_VOLTAGE)         \
+    .payload(b'\x02')                             \
+    .build()
+
+battery_ampere = Message()                        \
+    .direction(Direction.MASTER_TO_BATTERY)       \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.BATTERY_CURRENT)         \
+    .payload(b'\x02')                             \
+    .build()
+
+battery_percentage = Message()                    \
+    .direction(Direction.MASTER_TO_BATTERY)       \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.BATTERY_PERCENT)         \
+    .payload(b'\x02')                             \
+    .build()
+
+battery_cell_voltages = Message()                 \
+    .direction(Direction.MASTER_TO_BATTERY)       \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.BATTERY_CELL_VOLTAGES)   \
+    .payload(b'\x1B')                             \
+    .build()
+
+trip_distance = Message()                \
+    .direction(Direction.MASTER_TO_MOTOR)         \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.TRIP_DISTANCE)  \
+    .payload(b'\x02')                             \
+    .build()
+
+distance_left = Message()                         \
+    .direction(Direction.MASTER_TO_MOTOR)         \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.DISTANCE_LEFT)           \
+    .payload(b'\x02')                             \
+    .build()
+
+speed = Message()                                 \
+    .direction(Direction.MASTER_TO_MOTOR)         \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.SPEED)                   \
+    .payload(b'\x02')                             \
+    .build()
+
+tail_light_status = Message()                     \
+    .direction(Direction.MASTER_TO_MOTOR)         \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.TAIL_LIGHT)              \
+    .payload(b'\x02')                             \
+    .build()
+
+turn_on_tail_light = Message()                    \
+    .direction(Direction.MASTER_TO_MOTOR)         \
+    .read_write(ReadWrite.WRITE)                  \
+    .attribute(Attribute.TAIL_LIGHT)              \
+    .payload(b'\x02\x00')                         \
+    .build()
+
+turn_off_tail_light = Message()                   \
+    .direction(Direction.MASTER_TO_MOTOR)         \
+    .read_write(ReadWrite.WRITE)                  \
+    .attribute(Attribute.TAIL_LIGHT)              \
+    .payload(b'\x00\x00')                         \
+    .build()
+
+cruise_status = Message()                         \
+    .direction(Direction.MASTER_TO_MOTOR)         \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.CRUISE)                  \
+    .payload(b'\x02')                             \
+    .build()
+
+general_info = Message()                          \
+    .direction(Direction.MASTER_TO_MOTOR)         \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.GENERAL_INFO)            \
+    .payload(b'\x16')                             \
+    .build()
+
+trip_info = Message()                             \
+    .direction(Direction.MASTER_TO_MOTOR)         \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.TRIP_INFO)               \
+    .payload(b'\x0A')                             \
+    .build()
+
+motor_info = Message()                            \
+    .direction(Direction.MASTER_TO_MOTOR)         \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.MOTOR_INFO)              \
+    .payload(b'\x20')                             \
+    .build()
+
+battery_info = Message()                          \
+    .direction(Direction.MASTER_TO_BATTERY)       \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.BATTERY_INFO)            \
+    .payload(b'\x0A')                             \
+    .build()
+
+supplementary = Message()                         \
+    .direction(Direction.MASTER_TO_BATTERY)       \
+    .read_write(ReadWrite.READ)                   \
+    .attribute(Attribute.SUPPLEMENTARY)           \
+    .payload(b'\x06')                             \
+    .build()
+
--- m365py/m365py.py
+++ m365py/m365py.py
@@ -1,3 +1,5 @@
+from .m365message import *
+
 from typing import Callable, Iterable, Optional
 from collections import namedtuple
 
@@ -5,20 +7,10 @@ import struct
 import time
 import json
 import logging
-import M365Message
 
 from bluepy.btle import Peripheral, Characteristic, UUID, DefaultDelegate, ADDR_TYPE_RANDOM
 
-stream_handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-stream_handler.setFormatter(formatter)
-
 log = logging.getLogger('m365py')
-log.setLevel(logging.DEBUG)
-log.addHandler(stream_handler)
-
-def phex(s):
-    return ''.join('/x{:02x}'.format(x) for x in s)
 
 class M365Delegate(DefaultDelegate):
     def __init__(self, m365):
@@ -28,6 +20,12 @@ class M365Delegate(DefaultDelegate):
 
         self.motor_info_first_part = None
         self.general_info_first_part = None
+
+        stream_handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        stream_handler.setFormatter(formatter)
+
+        log.addHandler(stream_handler)
 
     @staticmethod
     def unpack_to_dict(fields, unpacked_tuple):
@@ -39,10 +37,9 @@ class M365Delegate(DefaultDelegate):
 
     def handle_message(self, message):
         log.debug("Received message: {}".format(message.as_dict()))
-        log.debug("Payload: {}".format(phex(message._payload)))
 
         result = {}
-        if message._attribute == M365Message.Attribute.DISTANCE_LEFT:
+        if message._attribute == Attribute.DISTANCE_LEFT:
             result = M365Delegate.unpack_to_dict(
                 'distance_left_km',
                 struct.unpack('<H', message._payload)
@@ -50,31 +47,29 @@ class M365Delegate(DefaultDelegate):
 
             result['distance_left_km'] /= 100  # km
 
-        elif message._attribute == M365Message.Attribute.SPEED:
+        elif message._attribute == Attribute.SPEED:
             result = M365Delegate.unpack_to_dict(
                 'speed_kmh',
-                struct.unpack('<H', message._payload)
+                struct.unpack('<h', message._payload)
             )
 
             result['speed_kmh'] /= 100
 
-        elif message._attribute == M365Message.Attribute.DISTANCE_SINCE_STARTUP:
+        elif message._attribute == Attribute.TRIP_DISTANCE:
             result = M365Delegate.unpack_to_dict(
-                'distance_since_startup_km',
-                struct.unpack('<H', message._payload[6:8])
+                'trip_distance_m',
+                struct.unpack('<H', message._payload)
             )
 
-            result['distance_since_startup_km'] /= 1000
-
-        elif message._attribute == M365Message.Attribute.TAIL_LIGHT:
+        elif message._attribute == Attribute.TAIL_LIGHT:
             is_light_on = message._payload[0] == 0x02
             result = {'is_tail_light_on': is_light_on}
 
-        elif message._attribute == M365Message.Attribute.CRUISE:
+        elif message._attribute == Attribute.CRUISE:
             is_cruise_on = message._payload[0] == 0x01
             result = {'is_cruise_on': is_cruise_on}
 
-        elif message._attribute == M365Message.Attribute.BATTERY_INFO:
+        elif message._attribute == Attribute.BATTERY_INFO:
             result = M365Delegate.unpack_to_dict(
                 'battery_capacity battery_percent battery_current battery_voltage battery_temperature_1 battery_temperature_2',
                 struct.unpack('<HHhHBB', message._payload)
@@ -86,7 +81,7 @@ class M365Delegate(DefaultDelegate):
             result['battery_temperature_1'] -= 20   # C
             result['battery_temperature_2'] -= 20   # C
 
-        elif message._attribute == M365Message.Attribute.BATTERY_VOLTAGE:
+        elif message._attribute == Attribute.BATTERY_VOLTAGE:
             result = M365Delegate.unpack_to_dict(
                 'battery_voltage',
                 struct.unpack('<H', message._payload)
@@ -94,7 +89,7 @@ class M365Delegate(DefaultDelegate):
 
             result['battery_voltage'] /= 100 # V
 
-        elif message._attribute == M365Message.Attribute.BATTERY_CURRENT:
+        elif message._attribute == Attribute.BATTERY_CURRENT:
             result = M365Delegate.unpack_to_dict(
                 'battery_current',
                 struct.unpack('<h', message._payload)
@@ -102,28 +97,28 @@ class M365Delegate(DefaultDelegate):
 
             result['battery_current'] /= 100 # A
 
-        elif message._attribute == M365Message.Attribute.BATTERY_PERCENT:
+        elif message._attribute == Attribute.BATTERY_PERCENT:
             result = M365Delegate.unpack_to_dict(
                 'battery_percent',
                 struct.unpack('<H', message._payload)
             )
 
-        elif message._attribute == M365Message.Attribute.GENERAL_INFO:
+        elif message._attribute == Attribute.GENERAL_INFO:
             #          [                      SERIAL                          ][          PIN         ][ VER  ]
             # payload: /x31/x36/x31/x33/x32/x2f/x30/x30/x30/x39/x35/x32/x39/x32/x30/x30/x30/x30/x30/x30/x38/x01
             result = M365Delegate.unpack_to_dict(
                 'serial pin version',
                 struct.unpack('<14s6sH', message._payload)
             )
-            result['serial']  = str(result['serial'])
-            result['pin']     = str(result['pin'])
+            result['serial']  = result['serial'].decode('utf-8')
+            result['pin']     = result['pin'].decode('utf-8')
             result['version'] = '{:02x}'.format(result['version'])
             result['version'] = 'V' + result['version'][0] + '.' + result['version'][1] + '.' + result['version'][2]
 
-        elif message._attribute == M365Message.Attribute.MOTOR_INFO:
+        elif message._attribute == Attribute.MOTOR_INFO:
             result = M365Delegate.unpack_to_dict(
                 'error warning flags workmode battery_percent speed_kmh speed_average_kmh odometer_km trip_distance_m uptime_s frame_temperature',
-                struct.unpack('<HHHHHHHIhhhxxxxxxxx', message._payload)
+                struct.unpack('<HHHHHhHIhhhxxxxxxxx', message._payload)
             )
 
             result['speed_kmh']         /= 100  # km /h
@@ -131,7 +126,7 @@ class M365Delegate(DefaultDelegate):
             result['odometer_km']       /= 1000 # km
             result['frame_temperature'] /= 10   # °C
 
-        elif message._attribute == M365Message.Attribute.TRIP_INFO:
+        elif message._attribute == Attribute.TRIP_INFO:
             #          [uptime][]
             # payload: xec/x00 /x00/x00/x00/x00/x00/x00/xe6/x00
             result = M365Delegate.unpack_to_dict(
@@ -141,9 +136,10 @@ class M365Delegate(DefaultDelegate):
 
             result['frame_temperature'] /= 10   # °C
 
-        elif message._attribute == M365Message.Attribute.BATTERY_CELL_VOLTAGES:
+        elif message._attribute == Attribute.BATTERY_CELL_VOLTAGES:
             #          [cell1 ][cell2 ]                     ...                                [cell10][           ???            ]
             # payload: /x2d/x10/x2e/x10/x1d/x10/x2f/x10/x34/x10/x34/x10/x3a/x10/x3a/x10/x2e/x10/x2f/x10/x00/x00/x00/x00/x00/x00/x00
+            print(phex(message._payload))
             result = M365Delegate.unpack_to_dict(
                 '''cell_1_voltage cell_2_voltage cell_3_voltage cell_4_voltage
                 cell_5_voltage cell_6_voltage cell_7_voltage cell_8_voltage
@@ -154,6 +150,14 @@ class M365Delegate(DefaultDelegate):
             for key, value in result.items():
                 result[key] = value / 100 # V
 
+        elif message._attribute == Attribute.SUPPLEMENTARY:
+            #          [ kers ] [cruise] [taillight]
+            # payload: /x00/x00 /x00/x00 /x00/x00
+            result = M365Delegate.unpack_to_dict(
+                'kers cruise_mode tail_light',
+                struct.unpack('<HHH', message._payload)
+            )
+
         else:
             log.warning('Unhandled message!')
             return
@@ -161,29 +165,30 @@ class M365Delegate(DefaultDelegate):
         # write result to m365 cached state
         for key, value in result.items():
             # hacky way of writing key and value to state
-            self._m365.state.__dict__[key] = value
+            self._m365.cached_state[key] = value
 
         # call user callback
         if self._m365._callback:
             self._m365._callback(self._m365, message, result)
 
 
-
     def handleNotification(self, cHandle, data):
         data = bytes(data)
+        log.debug('Got raw bytes: {}'.format(phex(data)))
 
         # sometimes we receive empty payload, ignore these
         if len(data) == 0: return
-        parse_status, message = M365Message.Message.parse_from_bytes(data)
+        parse_status, message = Message.parse_from_bytes(data)
 
-        if parse_status == M365Message.ParseStatus.OK:
+        if parse_status == ParseStatus.OK:
             self.handle_message(message)
 
-        elif parse_status == M365Message.ParseStatus.DISJOINTED:
+        elif parse_status == ParseStatus.DISJOINTED:
             self.disjointed_messages.append(data)
 
-        elif parse_status == M365Message.ParseStatus.INVALID_HEADER:
+        elif parse_status == ParseStatus.INVALID_HEADER:
             # This could mean we got rest of disjointed message
+            # TODO: if no matching message was found... warn the user
             for i, prev_data in enumerate(self.disjointed_messages):
                 combined_data = bytearray()
                 combined_data.extend(prev_data)
@@ -191,34 +196,10 @@ class M365Delegate(DefaultDelegate):
                 combined_data = bytes(combined_data)
 
                 # try parse combined data
-                parse_status, message = M365Message.Message.parse_from_bytes(combined_data)
-                if parse_status == M365Message.ParseStatus.OK:
+                parse_status, message = Message.parse_from_bytes(combined_data)
+                if parse_status == ParseStatus.OK:
                     self.handle_message(message)
                     del self.disjointed_messages[i]
-
-
-
-class M365State():
-    speed_kmh                  = None
-    speed_average_kmh          = None
-    distance_left_km           = None
-    odometer_km                = None
-    distance_since_startup_km  = None
-    frame_temperature          = None # °C
-    is_light_on                = None # bool
-    is_in_cruise_mode          = None
-
-    battery_percent        = None # %
-    battery_voltage        = None # V
-    battery_capacity       = None # Ah
-    battery_current        = None # A
-    battery_temperature_1  = None # °C
-    battery_temperature_2  = None # °C
-
-    def as_dict(self): return self.__dict__
-
-    def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 
 class M365(Peripheral):
@@ -229,7 +210,7 @@ class M365(Peripheral):
         Peripheral.__init__(self)
         self.mac_address = mac_address
 
-        self.state = M365State()
+        self.cached_state = {}
         self._callback = callback
 
     @staticmethod
