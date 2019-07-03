a323657f11e4933ea7505aa52030b5a70ec00649
AntonHakansson <anton.hakansson98@gmail.com>
2019-07-03 13:54:59 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='m365py',
+    url='https://github.com/AntonHakansson/m365py',
+    author='Anton Håkansson',
+    author_email='anton.hakansson98@@gmail.com',
+    packages=['m356py'],
+    install_requires=['bluepy'],
+    version='0.1',
+    license='MIT',
+    description='A library to receive parsed BLE Xiaomi M365 scooter(Version=V1.3.8) messages using bluepy',
+    # TODO:
+    # long_description=open('README.md').read(),
+)
